1fb755071dda1d58674d79d9341d9ddfe56d8459
Hokutosei <jeanepaul@gmail.com>
2016-06-13 17:33:30 +0900

    changed time format to rfc822z

--- logger.go
+++ logger.go
@@ -32,7 +32,7 @@ func logLevel(key string) string {
 }
 
 func timeLoglevel(logLevelStr string) string {
-	str := fmt.Sprintf("[%s] %s", time.Now().Format("Mon Jan _2 15:04:05 2006"), logLevel(logLevelStr))
+	str := fmt.Sprintf("[%s] %s", time.Now().Format(time.RFC822Z), logLevel(logLevelStr))
 	return str
 }
 
