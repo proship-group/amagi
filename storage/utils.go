66e4abb6fd5b52b303209c7b095d199ec3f2f0ce
hokutosei <jeanepaul@gmail.com>
2017-02-07 20:16:38 +0900

    update bytes encoded to base64 with panic error handler

--- storage/utils.go
+++ storage/utils.go
@@ -2,15 +2,19 @@ package storage
 
 import (
 	"bytes"
+	"encoding/base64"
 	"mime/multipart"
 )
 
 // EncodeFileToBase64 encode file(io.Reader) to base64
-func EncodeFileToBase64(file multipart.File) error {
+func EncodeFileToBase64(file multipart.File) string {
 	b := new(bytes.Buffer)
-
 	b.ReadFrom(file)
-	b.Bytes()
 
-	return nil
+	// TODO better error handler! -JP
+	if len(b.Bytes()) == 0 {
+		panic("empty file!")
+	}
+
+	return base64.StdEncoding.EncodeToString(b.Bytes())
 }
