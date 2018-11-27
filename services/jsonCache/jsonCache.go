a730610b31d0483dec0806c6ae367b8d93e9cd7b
Hokutosei <jeanepaul@gmail.com>
2018-11-27 20:26:55 +0900

    port of json cache from apicore

---
+++ services/jsonCache/jsonCache.go
@@ -0,0 +1,84 @@
+package jsonCache
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	// "linkermodels"
+
+	utils "github.com/b-eee/amagi"
+)
+
+var (
+	separator = ":"
+)
+
+// JSONStringify stringify json data
+func JSONStringify(data interface{}) (string, error) {
+	str, err := json.Marshal(data)
+	if err != nil {
+		utils.Info(fmt.Sprintf("error JSONStringify %v", err))
+		return "", err
+	}
+
+	return string(str), nil
+}
+
+// CacheSetEx simple cache json data to string value, ttl in seconds
+func CacheSetEx(value interface{}, keys []string, ttl int) error {
+	s := time.Now()
+	strVal, err := JSONStringify(value)
+	if err != nil {
+		return err
+	}
+
+	if err := SetEx(joinKeysToSTR(keys...), strVal, ttl); err != nil {
+		utils.Info(fmt.Sprintf("JSONCacheSet %v", err))
+		return err
+	}
+
+	return utils.Info(fmt.Sprintf("JSONCacheSet took: %v key: %v ===============+================", time.Since(s), keys))
+}
+
+// CacheGetEx get string cache and convert to json
+func CacheGetEx(keys []string, sult interface{}, ttl int) error {
+	s := time.Now()
+	str, err := GetEx(joinKeysToSTR(keys...), ttl)
+	if err != nil {
+		utils.Info(fmt.Sprintf("JSONCacheGetEx Hit miss! %v", err))
+		return err
+	}
+
+	var result interface{}
+	// fmt.Println(string(str))
+	if err := json.Unmarshal(str, &result); err != nil {
+		utils.Info(fmt.Sprintf("error unmarshal json cache string %v", err))
+		fmt.Println("-------------------ssss------------ssss------------ssss------------ssss------------ssss")
+		return err
+	}
+
+	fmt.Println("--------------------xxxx----------------xxxx----------------xxxx----------------xxxx----------------xxxx----------------xxxx----------------xxxx")
+	fmt.Println(result)
+	fmt.Println("--------------------xxxx----------------xxxx----------------xxxx----------------xxxx----------------xxxx----------------xxxx----------------xxxx")
+
+	return utils.Info(fmt.Sprintf("JSONCacheGetEx Hit! took: %v keys: %v", time.Since(s), joinKeysToSTR(keys...)))
+}
+
+// CacheDelete json cache delete
+func CacheDelete(keys []string) error {
+	s := time.Now()
+
+	if err := Delete(joinKeysToSTR(keys...)); err != nil {
+		return err
+	}
+
+	utils.Info(fmt.Sprintf("JSONCacheDelete took %v", time.Since(s)))
+	return nil
+}
+
+func joinKeysToSTR(keys ...string) string {
+
+	return strings.Join(keys, ":")
+}
---
+++ services/jsonCache/redis.go
@@ -0,0 +1,138 @@
+package jsonCache
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/b-eee/amagi/services/database"
+
+	utils "github.com/b-eee/amagi"
+	"github.com/garyburd/redigo/redis"
+)
+
+var (
+	// EnableCacheEnv enable cache environment variable
+	EnableCacheEnv = "ENABLE_REDIS_CACHE"
+
+	// KeyExpiration max key Time To Live seconds
+	KeyExpiration = 10000000
+)
+
+// Set set key and value string to redis
+func Set(key, value string) error {
+	if os.Getenv(EnableCacheEnv) != "1" {
+		return fmt.Errorf("redis Set cache disabled")
+	}
+
+	s := time.Now()
+	c := database.GetRedisConn()
+	defer c.Close()
+
+	if _, err := c.Do("SET", key, value); err != nil {
+		utils.Error(fmt.Sprintf("erro Redis Set %v", err))
+		return err
+	}
+
+	return utils.Info(fmt.Sprintf("Set took: %v", time.Since(s)))
+}
+
+// SetEx set KV with expire
+func SetEx(key, value string, ttl int) error {
+	if os.Getenv(EnableCacheEnv) != "1" {
+		return fmt.Errorf("redis SetEx cache disabled")
+	}
+
+	s := time.Now()
+	c := database.GetRedisConn()
+	defer c.Close()
+
+	if err := c.Send("MULTI"); err != nil {
+		utils.Error(fmt.Sprintf("error Multi %v", err))
+		return err
+	}
+
+	if err := c.Send("SET", key, value); err != nil {
+		utils.Error(fmt.Sprintf("error Setex %v", err))
+		return err
+	}
+	if err := c.Send("EXPIRE", key, ttl); err != nil {
+		utils.Error(fmt.Sprintf("error EXPIRE %v", err))
+		return err
+	}
+
+	if _, err := c.Do("EXEC"); err != nil {
+		utils.Error(fmt.Sprintf("error EXEC %v", err))
+		return err
+	}
+
+	utils.Info(fmt.Sprintf("SetEx took: %v", time.Since(s)))
+	return nil
+}
+
+// GetEx get kv and update expire
+func GetEx(key string, ttl int) ([]uint8, error) {
+	if os.Getenv(EnableCacheEnv) != "1" {
+		return []uint8{}, fmt.Errorf("redis SetEx cache disabled")
+	}
+
+	s := time.Now()
+	c := database.GetRedisConn()
+	defer c.Close()
+
+	if err := c.Send("MULTI"); err != nil {
+		return []uint8{}, utils.Error(fmt.Sprintf("error Multi %v", err))
+	}
+
+	if err := c.Send("EXPIRE", key, ttl); err != nil {
+		return []uint8{}, utils.Error(fmt.Sprintf("error EXPIRE %v", err))
+	}
+
+	if err := c.Send("GET", key); err != nil {
+		return []uint8{}, utils.Error(fmt.Sprintf("error in GET getex %v", err))
+	}
+
+	str, err := redis.Values(c.Do("EXEC"))
+	if err != nil || str[1] == nil {
+		fmt.Println(str[1] == nil)
+		fmt.Println("----------------------------------oooooooooooooooooooooo------------oooooooooooooooooooooo------------oooooooooooooooooooooo------------oooooooooooooooooooooo")
+		return []uint8{}, utils.Error(fmt.Sprintf("error EXEC inGetEx %v", err))
+	}
+
+	return str[1].([]byte), utils.Info(fmt.Sprintf("GetEx took: %v", time.Since(s)))
+}
+
+// Get get value from redis
+func Get(key string) (string, error) {
+	if os.Getenv(EnableCacheEnv) != "1" {
+		return "", fmt.Errorf("redis Get cache disabled")
+	}
+
+	c := database.GetRedisConn()
+	defer c.Close()
+
+	str, err := redis.String(c.Do("GET", key))
+	if err != nil {
+		utils.Info(fmt.Sprintf("error Redis Get %v", err))
+		return "", err
+	}
+
+	return str, nil
+}
+
+// Delete delete key w/ values
+func Delete(key string) error {
+	if os.Getenv(EnableCacheEnv) != "1" {
+		return fmt.Errorf("redis Delete cache disabled")
+	}
+	c := database.GetRedisConn()
+	defer c.Close()
+
+	_, err := c.Do("DEL", key)
+	if err != nil {
+		utils.Error(fmt.Sprintf("error Delete REdis key %v", err))
+		return err
+	}
+
+	return nil
+}
